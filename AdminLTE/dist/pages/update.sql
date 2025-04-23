7106f04fabe5ac69d34fcfe4d0ef6c69db195d47
BICCOUNT-TECHNOLOGIES <github@biccounttechnologies.com>
2025-04-23 17:32:11 +0300

    Recent

--- AdminLTE/dist/pages/update.sql
+++ AdminLTE/dist/pages/update.sql
@@ -3,3 +3,5 @@ Created a connection page to the database . by michelle.
 
 Created the Users Table . by nucho.
 here are the columns - id	name	email	phone	password	avatar	role	status	email_verified_at	last_login	created_at	updated_at
+
+Created the Building  Table .  by michelle
