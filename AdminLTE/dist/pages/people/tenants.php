5431f14e148fe77dbc17c37a9edd7ce8d2ac62a9
BICCOUNT-TECHNOLOGIES <github@biccounttechnologies.com>
2025-04-28 13:12:41 +0300

    Recent

--- AdminLTE/dist/pages/people/tenants.php
+++ AdminLTE/dist/pages/people/tenants.php
@@ -5,7 +5,7 @@
 
 <?php
 try {
-  $conn = new PDO("mysql:host=$host;dbname=$db");
+  $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   // Fetch tenants with their user details
