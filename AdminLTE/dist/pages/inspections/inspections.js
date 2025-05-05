6755c5165358904318d21df059a38771356df16a
manucho661 <emmanuelwanyonyi49@gmail.com>
2025-05-05 12:25:31 +0300

    Recent

--- AdminLTE/dist/pages/inspections/inspections.js
+++ AdminLTE/dist/pages/inspections/inspections.js
@@ -5,4 +5,41 @@ function addNewSchedule() {
   content.style.display = 'flex'; // or 'flex'/'inline' depending on your layout
 }
 
+// custom select container
+document.querySelectorAll('.select-option-container').forEach(container => {
 
+         const select = container.querySelector('.custom-select');
+        const optionsContainer = container.querySelector('.select-options');
+        const options = optionsContainer.querySelectorAll("div");
+        select.addEventListener("click", () => {
+          const isOpen = optionsContainer.style.display === "block";
+
+              optionsContainer.style.display = isOpen ? "none" : "block";
+              select.style.borderRadius = isOpen ? "5px" : "5px 5px 0 0";
+
+              // This line is key: it adds or removes the "open" class
+              select.classList.toggle("open", !isOpen);
+
+        });
+
+        options.forEach(option => {
+           option.addEventListener("click", () => {
+            select.textContent = option.textContent;
+            select.setAttribute("data-value", option.getAttribute("data-value"));
+            options.forEach(opt => opt.classList.remove("selected"));
+            option.classList.add("selected");
+            optionsContainer.style.display = "none";
+            select.style.borderRadius = "5px";
+            select.classList.remove("open");
+
+      });
+    });
+    document.addEventListener("click", (e) => {
+      if (!e.target.closest(".select-option-container")) {
+        optionsContainer.style.display = "none";
+        select.style.borderRadius = "5px";
+        select.classList.remove("open");
+      }
+
+    })
+});
