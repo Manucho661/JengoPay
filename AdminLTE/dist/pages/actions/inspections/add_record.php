06bd0e7fc30d3d3f4ae08133cb7cbf028cc6ec53
Manucho661 <emmanuelwanyonyi49@gmail.com>
2025-05-21 08:38:11 +0300

    Recent

--- AdminLTE/dist/pages/actions/inspections/add_record.php
+++ AdminLTE/dist/pages/actions/inspections/add_record.php
@@ -3,21 +3,21 @@
  include '../../db/connect.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    
-                
+
+
                 $building_name = $_POST['building_name'] ?? '';
                 $unit_name = $_POST['unit_name'] ?? '';
                 $inspection_type = $_POST['inspection_type'] ?? '';
                 $date = $_POST['date'] ?? '';
-                
+
 
                 if ( $building_name && $unit_name && $inspection_type && $date) {
                     try {
                         $pdo->beginTransaction();
 
                         // Step 1: Insert into users
-                        $stmtUser = $pdo->prepare("INSERT INTO inspections ( building_name , inspection_type, date) VALUES (?,?, ?, ?)");
-                        $stmtUser->execute([$inspection_number, $building_name,  $unit_name, $date]);
+                        $stmtUser = $pdo->prepare("INSERT INTO inspections ( building_name , inspection_type, date) VALUES (?,?, ?)");
+                        $stmtUser->execute([ $building_name,  $unit_name, $date]);
                         $user_id = $pdo->lastInsertId();
                         $pdo->commit();
                         echo "New Schedule added successfully!";
@@ -28,7 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 } else {
                     echo "All New Inspection fields are required.";
                 }
-       
+
     }
  else {
     echo "Invalid request.";
