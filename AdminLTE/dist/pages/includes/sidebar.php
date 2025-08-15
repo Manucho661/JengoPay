d4821449c6d835c2f6eac35b1f7b6b8abda8d8f7
BICCOUNT-TECHNOLOGIES <github@biccounttechnologies.com>
2025-08-15 17:51:54 +0300

    Recent

--- AdminLTE/dist/pages/includes/sidebar.php
+++ AdminLTE/dist/pages/includes/sidebar.php
@@ -31,12 +31,10 @@
     <div class="submenu">
     <a href="/OriginalTwo/AdminLTE/dist/pages/communications/texts.php">💬 In-app Messages</a>
       <a href="/OriginalTwo/AdminLTE/dist/pages/communications/announcements.php">📢 Announcements</a>
-
     </div>
   </div>
-
+  
   <a href="/OriginalTwo/AdminLTE/dist/pages/inspections/inspections.php">🕵️ Inspections</a>
-  <a href="/OriginalTwo/AdminLTE/dist/pages/rent/rent.php">💸 Rent</a>
   <a href="/OriginalTwo/AdminLTE/dist/pages/maintenance/maintenance.php">🛠 Repairs & Maintenance</a>
 
   <!-- Financials -->
@@ -45,6 +43,7 @@
       <span class="arrow" style="color: #FFC107 !important;"><i class="bi bi-caret-right-fill arrow"></i></span> 💼 Financials
     </div>
     <div class="submenu">
+      <a href="/OriginalTwo/AdminLTE/dist/pages/financials/Rent.php">💸Rent</a>
       <a href="/OriginalTwo/AdminLTE/dist/pages/financials/expenses/expenses.php">💵 Expenses</a>
       <a href="/OriginalTwo/AdminLTE/dist/pages/financials/cashflow.php">💵 Cashflow</a>
       <a href="/OriginalTwo/AdminLTE/dist/pages/financials/invoice.php">📄 Invoices</a>
