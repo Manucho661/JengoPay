3412836d2c4c5c30dfa547e8f789acc3b7f44fcd
MichyMwesh <michtymwesh@gmail.com>
2025-12-09 07:45:20 +0300

    Recent

--- landlord/pages/Buildings/buildings.php
+++ landlord/pages/Buildings/buildings.php
@@ -363,6 +363,7 @@ require_once "../db/connect.php"
                                 </div>
                             </div>
                         </div>
+                                </div>
                         <div class="card-body">
                             <div class="table-responsive">
                                 <table class="table table-hover" id="dataTable">
